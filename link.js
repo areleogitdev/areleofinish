27cd585b9a96c7d1fd865a247d1c0bbb0fca1ff9
areleogitdev <n0w.a7ch@gmail.com>
2017-10-02 15:18:06 +0700

    Initial commithhhhhhhhhhhhhhhhhhhhhh

---
+++ link.js
@@ -0,0 +1,93 @@
+
+function make(Schema, mongoose, app) {
+    // Define Car model
+	
+	console.log(Schema+mongoose);
+	app.get('/updateadmin',function(req,res){
+  res.sendfile("demos/admin/updateadmin.html");
+});
+	
+	
+}
+
+
+function makedata(Schema, mongoose, app) {
+    // Define Car model
+app.get('/',function(req,res){
+  res.sendfile("demos/loginr.html");
+});
+		
+app.get('/login',function(req,res){
+  res.sendfile("demos/loginr.html");
+});
+
+app.get('/testdata',function(req,res){
+  res.sendfile("demos/test.html");
+});	
+
+app.get('/create',function(req,res){
+  res.sendfile("demos/create.html");
+});
+
+app.get('/update',function(req,res){
+  res.sendfile("demos/update.html");
+});
+
+app.get('/delete',function(req,res){
+  res.sendfile("demos/delete.html");
+});
+
+app.get('/insertdata',function(req,res){
+  res.sendfile("demos/insert.html");
+});
+
+app.get('/updatedata',function(req,res){
+  res.sendfile("demos/update.html");
+});
+
+app.get('/deletedata',function(req,res){
+  res.sendfile("demos/delete.html");
+});
+
+//admin
+app.get('/createadmin',function(req,res){
+  res.sendfile("demos/admin/createadmin.html");
+});
+
+app.get('/readadminr',function(req,res){
+  res.sendfile("demos/admin/readadmin.html");
+});	
+
+app.get('/updateadmin',function(req,res){
+  res.sendfile("demos/admin/updateadmin.html");
+});
+
+app.get('/deleteadmin',function(req,res){
+  res.sendfile("demos/admin/deleteadmin.html");
+});
+
+
+app.get('/createuseradmin',function(req,res){
+  res.sendfile("demos/admin/createuseradmin.html");
+});
+
+app.get('/loginadmin',function(req,res){
+  res.sendfile("demos/admin/loginadmin.html");
+});
+
+
+
+
+
+app.get('/dashboardadmin',function(req,res){
+  res.sendfile("demos/admin/dashboard.html");
+});
+
+
+
+	
+	
+}
+
+module.exports.make = make;
+module.exports.makedata = makedata;
